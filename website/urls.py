35a78032f22d8f90cd2a96c8d754d6f691f54f3e
sabaul <sabaul0801@gmail.com>
2023-04-09 17:33:13 +0530

    taking a break

---
+++ website/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    # path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+]
